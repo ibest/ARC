bcb14763ec60d828cd6005b9c4ecd8a6432486a1
Brice Sarver <bsarver1337@gmail.com>
2013-05-31 16:14:52 -0700

    spades assembly call

--- ARC/assembly_spades.py
+++ ARC/assembly_spades.py
@@ -1,35 +1,53 @@
-#assembly.py
-#modular python script to pass arguments to the assembler and manage output
+#assembly_spades.py
+#spades subprocess calls and exit status management
+
+import os
+import subprocess
+from ARC import exceptions
 
 
-from optparse import OptionParser
-import os, re, math
+params = {'t': 5, 'PE1': "phiX_S1_L001_R1_001_first15k.fastq", 'PE2': "phiX_S1_L001_R2_001_first15k.fastq", 'o': "spades_test"}
 
+# 'PE1': "PE1.fastq", 'PE2': "PE2.fastq", 'SE': "SE.fastq", 
 
-params = {'PE1': 'PE1.fastq', 'PE2': 'PE2.fastq', 'SE': 'SE.fastq', 't': 5}
+#'PE1': "phiX_S1_L001_R1_001_first15k.fastq"
 
-def run_spades(params):
+print params
+
+def RunSpades(params):
+
 	"""
-	Several arguments can be passed to spades.py: -1 [PE1], -2 [PE2], -s [SE], -t [# of threads]
+	Several arguments can be passed to spades.py: -1 [PE1], -2 [PE2], -s [SE], and -o [target_dir]
 	"""
+	#Check that required params are available	
+	if not ('PE1' in params and 'PE2' in params) or ('SE' in params):
+		raise exceptions.FatalException('Missing params in RunSpades.')
+
+	#Check that the files actually exist
+	if 'PE1' in params and 'PE2' in params and not(os.path.exists(params['PE1']) and os.path.exists(params['PE2'])):
+		raise exceptioins.FatalException('Missing PE files in RunSpades.')
+		
+	if 'SE' in params and not(os.path.exists(params['SE'])):
+		raise exceptioins.FatalException('Missing SE files in RunSpades.')
 	
+	#Build args for assembler call		
+	args = ['spades.py']
+	if 'PE1' in params and 'PE2' in params:
+		args += ['-1', params['PE1'], '-2', params['PE2']]
+		
+	if 'SE' in params:
+		args += ['-s', params['SE']]
 	
+	args += ['-o', params['target_dir', '-t', '1']]
+	
+	if 'verbose' in params:
+		out = open(os.path.join(params['target_dir'], "assembly.log"), 'w')
+		
+	else:
+		out = open(os.devnull, 'w')
 
-
-parser = OptionParser(usage = "usage: python %prog [-f FILENAME] [-o output_file_name]", version = "%prog 0.1")
-
-parser.add_option("-t", "--threads", dest="threads", help="Number of threads to run SPADES on.")
-
-
-
-
-
-
-parser.add_option("-f", "--file", dest="filename", help="Path to the *.codeml file to be parsed")
-parser.add_option("-o", "--outfile", dest="outfile", help="Output File")
-(options, args) = parser.parse_args()
-if not options.filename and not options.outfile:
-	parser.error("You must specify a codeml output file and a file to write results to.")
-
-
-
+	ret = subprocess.call(args, stderr=out, stdout=out)
+	out.close()
+	
+	if ret != 0:
+		raise exceptions.RerunnableError("Assembly failed")
