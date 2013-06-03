3b8f179b76961c86b0a94be913bda6721413add6
Rob Lyon <nosignsoflifehere@gmail.com>
2013-06-03 09:06:47 -0700

    add setup configuration for installer

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='ARC',
+    version='0.0.1',
+    author='Institute for Bioinformatics and Evolutionary Studies',
+    url='http://www.ibest.uidaho.edu',
+    packages=['ARC'],
+    scripts=['bin/ARC']
+)
