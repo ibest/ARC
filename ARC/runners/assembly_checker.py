3f0bc92400430f49c1ce2911b3b971a8e344d513
Rob Lyon <nosignsoflifehere@gmail.com>
2013-07-24 00:08:15 -0700

    assembly_checker conversion to new runner

--- ARC/runners/assembly_checker.py
+++ ARC/runners/assembly_checker.py
@@ -16,32 +16,41 @@
 
 import os
 import time
-from ARC import logger
 from ARC.runner import Finisher
 from ARC.runner import BaseRunner
 
 
 class AssemblyChecker(BaseRunner):
     """
-    Checks for "finished" files in each of the assembly folders. Set values of params['assemblies'] to True for
-    all finished assemblies. If all assemblies are finished, kick off the finisher process.
+        Checks for "finished" files in each of the assembly folders. Set values
+        of params['assemblies'] to True for all finished assemblies. If all
+        assemblies are finished, kick off the finisher process.  Now that we
+        have job dependency, this will probably be deprecated.
+
     required params:
         'targets': dictionary, keys are paths, values are boolean
     """
     def execute(self):
-        """ run through list of targets, check any that haven't finished already """
+        """
+            run through list of targets, check any that haven't finished
+            already
+        """
         sample = self.params['sample']
         completed = sum(self.params['targets'].values())
-        logger.info("Sample: %s AssemblyChecker started with %s of %s targets completed" % (sample, completed, len(self.params['targets'])))
+        msg = "Sample: %s AssemblyChecker started with " % (sample)
+        msg += "%s of %s targets completed" % (
+            sample, completed, len(self.params['targets']))
+        self.info(msg)
         for target_folder in self.params['targets']:
             if not self.params['targets'][target_folder]:
                 file = os.path.join(target_folder, 'finished')
                 if os.path.exists(file):
                     self.params['targets'][target_folder] = True
-                    logger.info("%s exists" % file)
+                    self.info("%s exists" % file)
                     completed += 1
 
-        #Now check whether all have finished, if not, add a new AssemblyChecker to the queue
+        # Now check whether all have finished, if not, add a new
+        # AssemblyChecker to the queue
         if len(self.params['targets']) > sum(self.params['targets'].values()):
             # sleep 4 seconds before submitting
             time.sleep(5)
@@ -49,10 +58,16 @@ class AssemblyChecker(BaseRunner):
                 AssemblyChecker,
                 procs=1,
                 params=self.params)
-            logger.info("Sample: %s Assemblies not finished: %s of %s targets completed" % (sample, completed, len(self.params['targets'])))
+            msg = "Sample: %s Assemblies not finished: " % (sample)
+            msg += "%s of %s targets completed" % (
+                sample, completed, len(self.params['targets']))
+            self.info(msg)
         else:
             self.submit(
                 Finisher,
                 procs=1,
                 params=self.params)
-            logger.info("Sample: %s Assemblies finished: %s of %s targets completed" % (sample, completed, len(self.params['targets'])))
+            msg = "Sample: %s Assemblies finished: " % (sample)
+            msg += "%s of %s targets completed" % (
+                sample, completed, len(self.params['targets']))
+            self.info(msg)
