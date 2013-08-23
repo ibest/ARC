7e467ce8a8072eab57c73de3557fe0f8c4bf7e13
samhunter <shunter@gmail.com>
2013-08-23 16:06:21 -0700

    added reports to quick_info.sh

--- Extensions/quick_info.sh
+++ Extensions/quick_info.sh
@@ -1,11 +1,13 @@
 echo "Splits performed:"
 grep -c "Split" log.txt
+echo "Assembly reports:"
+grep -c "Assembly reports" log.txt
 echo "Assemblies finished:"
 grep -c "Assembly finished in" log.txt
 echo "Assemblies failed:"
 grep -c "Assembly failed after" log.txt
 echo "Assemblies killed:"
-grep -c "Assembly killed after" log.txt 
+grep -c "Assembly killed after" log.txt
 echo "Targets completed:"
 grep -c "did not incorporate any more reads" log.txt
 echo "Killed by repeat detection:"
